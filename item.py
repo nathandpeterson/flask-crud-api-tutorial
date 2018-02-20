126ffe9005d51ea75c30d26af65cce04f0982713
Nathan Daniel Peterson <natperson@gmail.com>
2018-02-20 11:18:06 -0800

    add get all method

--- item.py
+++ item.py
@@ -38,7 +38,15 @@ class Item(Resource):
         data = Item.parser.parse_args()
 
         item = {'name': name, 'price': data['price']}
+        try:
+            self.insert(item)
+        except:
+            return {'message': 'an error occurred'}, 500
 
+        return item, 201
+
+    @classmethod
+    def insert(cls, item):
         connection = sqlite3.connect('data.db')
         cursor = connection.cursor()
         query = 'INSERT INTO items VALUES(?,?)'
@@ -47,23 +55,42 @@ class Item(Resource):
         connection.commit()
         connection.close()
 
-        return item, 201
 
     def delete(self, name):
-        global items
-        items = list(filter(lambda x: x['name'] != name, items))
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = 'DELETE FROM item WHERE name=?'
+        cursor.execute(query, (name,))
+        connection.commit()
+        connection.close()
         return {'message': 'item deleted'}
 
     def put(self, name):
         data = Item.parser.parse_args()
 
-        item = next(filter(lambda x: x['name'] == name, items), None)
+        item = self.find_by_name(name)
+        updated_item = {'name': name, 'price': data['price']}
         if item == None:
-            item = {'name': name, 'price': data['price']}
-            items.append(item)
+            try:
+                self.insert(updated_item)
+            except:
+                return {'message': 'an error occurred'}, 500
         else:
-            item.update(data)
-        return item
+            try:
+                self.update(updated_item)
+            except:
+                return {'message': 'an error occurred'}, 500
+        return updated_item
+
+    @classmethod
+    def update(cls, item):
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+        query = 'UPDATE items SET price=? WHERE name=?'
+        cursor.execute(query, (item['price'], item['name']))
+        connection.commit()
+        connection.close()
+
 
 class ItemList(Resource):
     def get(self):
@@ -72,7 +99,8 @@ class ItemList(Resource):
 
         query = 'SELECT * from ITEMS'
         result = cursor.execute(query)
-        row = list(result.fetch())
-        print(row)
+        list = []
+        for row in result:
+            list.append({'name': row[0], 'price': row[1]})
         connection.close()
-        return {'items': row}
+        return {'items': list}
